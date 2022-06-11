6e54ab52dd8e67222e80c722e908248ebfba8490
DolgihPavel <dolgihgod@mail.ru>
2022-06-11 15:59:59 +0300

    changed to postgres database on heroku

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Долгих Павел, гр. 053504
 deployed here: https://library-isp-app.herokuapp.com/
+image.png
--- librarymanagement/settings.py
+++ librarymanagement/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -78,10 +79,7 @@ WSGI_APPLICATION = 'librarymanagement.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
 
 
@@ -165,3 +163,4 @@ LOGGING = {
     },
 }
 
+CSRF_TRUSTED_ORIGINS = ['https://library-isp-app.herokuapp.com']
